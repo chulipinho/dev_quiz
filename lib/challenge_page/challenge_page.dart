ffff91afb8407798ee397592addda51496102a44
chulipinho <fellipe.luz.machado@gmail.com>
2022-01-08 15:03:21 -0300

    Created next_button_widget

--- lib/challenge_page/challenge_page.dart
+++ lib/challenge_page/challenge_page.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge_page/widgets/next_button_widget.dart';
 import 'package:devquiz/challenge_page/widgets/question_indicator_widget.dart';
 import 'package:devquiz/challenge_page/widgets/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -21,6 +22,22 @@ class _ChallengePageState extends State<ChallengePage> {
         ),
       ),
       body: QuizWidget(title: "title"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(child: NextButtonWidget.white(label: "Pular", onTap: () {},)),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(child: NextButtonWidget.green(label: "Confirmar", onTap: () {},)),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:devquiz/challenge_page/challenge_page.dart';
 import 'package:devquiz/home_page/home_page.dart';
 import 'package:flutter/material.dart';
 
@@ -6,7 +7,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
